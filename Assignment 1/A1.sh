ba0c4ce4c554d6630e1f1a8d3555a0307c43f669
Pratham19Feb <51926984+Pratham19Feb@users.noreply.github.com>
2019-06-18 10:56:05 +0530

    Add files via upload

---
+++ Assignment 1/A1.sh
@@ -0,0 +1,17 @@
+echo -n -e "\n\n Welcome to First Program " 
+echo -n -e "\n\n\n 1) Create address book.
+			   \n 2) View address book.
+			   \n 3) Insert a record.
+			   \n 4) Delete a record.
+			   \n 5) Modify a record.
+			   \n 6) Exit."
+			   
+read choice
+
+case "$choice" in
+	
+ "1") echo "Enter Filename to be Create :  "
+ 	  read fname 	  
+ 
+
+esac
