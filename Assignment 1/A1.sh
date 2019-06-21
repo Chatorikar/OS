a3dac63cb3c3276cd0af5ab7164af1f2e6a944c2
Pratham19Feb <51926984+Pratham19Feb@users.noreply.github.com>
2019-06-21 10:59:01 +0530

    Update A1.sh

--- Assignment 1/A1.sh
+++ Assignment 1/A1.sh
@@ -1,17 +1,73 @@
-echo -n -e "\n\n Welcome to First Program " 
-echo -n -e "\n\n\n 1) Create address book.
-			   \n 2) View address book.
-			   \n 3) Insert a record.
-			   \n 4) Delete a record.
-			   \n 5) Modify a record.
-			   \n 6) Exit."
-			   
-read choice
-
-case "$choice" in
+header(){
 	
- "1") echo "Enter Filename to be Create :  "
- 	  read fname 	  
- 
+	echo -e "\t\t\t Name     Date of Journy   Source AirPort	 Destination AirPort   \n\n"
+	
+}
 
-esac
+Create(){
+
+	echo "Enter Filename  :  "
+	read filename 
+	touch $filename.txt	  
+	echo -n -e "\n\n Files $filename is Sucessfully Created  \n\n\n"
+		 	  
+}
+
+View_All_Record(){
+
+	echo -e "\t\t All Record are as follows :   \n\n\n"
+	header
+	cat R.txt
+		 	  
+}
+
+
+Insert_Record(){
+	
+	echo " Enter passanger Name : "
+	read name
+	echo " Hello ,  $name ! "
+	echo " Please Enter Date of Journey  : "
+	cal 
+	read date_journy  
+	echo "\n Please Enter Source : "
+	read source_AP
+	echo " Please Enter Destination  : " 
+	read Desti_AP
+		     
+	echo -e "\t\t\t $name        \t$date_journy June       \t$source_AP	      \t$Desti_AP   " >> R.txt
+
+}
+
+Delete_Record(){
+
+	echo " Enter Passanger Name : "
+	read name_delt
+	sed -i "/$name_delt/d" Record.txt
+}
+
+
+
+while true
+	do
+		echo -n -e "
+			    1) Create File.
+			    2) View all Passangers record.
+			    3) Insert a record.
+			    4) Delete a record.
+			    5) Modify a record.
+			    6) Exit.
+			    Enter Your Choice "
+		read choice
+		case $choice in
+	
+		 1)	Create;; 
+		 	  	  
+	 	 2) View_All_Record;;
+		      	 	  
+		 3) Insert_Record;;
+		 	   
+		 4) Delete_Record;;
+		 	 
+		esac
+	done
