f06d571674ee1f99baffa0089399d1e37664d363
Prathamesh J. Chatorikar <51926984+Pratham19Feb@users.noreply.github.com>
2019-07-30 10:57:03 +0530

    Update Sorting.c

--- Assignment 2/Sorting.c
+++ Assignment 2/Sorting.c
@@ -1,44 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <unistd.h>
 
 void Sort(int *arr , int len)
 {
-	
+
 	int temp;
-   
-    
+
+
     for(int i = 0 ; i < len ; i++){
-    
+
     	for(int j = i ; j < len ; j++){
-    		
+
     		if(arr[i] > arr[j]){
 
 				temp = arr[i];
 				arr[i] = arr[j];
-				arr[j] = temp;	
-    		
+				arr[j] = temp;
+
     		}
-    	
+
     	}
-    
+
     }
 
- 
- 
+
+
  printf("Sorted Array :");
- 
+
  for (int i = 0; i < len; i++){
-    	
-    	
+
+
     	printf("%d ", arr[i] );
     }
 
 }
 
 
-int main()
+int main(int argc , char *argv[])
 {
 
+	char *newargv[] = { NULL, "hello", "world", NULL };
+	 newargv[0] = argv[1];
 	int n = 0;
 	int arr[50];
     printf("Enter Size of the Array \n");
@@ -46,13 +49,18 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-    	printf("Enter Element a[%d] \n" , i );
+    	printf("Enter Element a[%d]  :  " , i );
     	scanf("%d" , &arr[i]);
     }
 
 
+
     Sort(arr,n);
 
+		if(argc != 2){
 
+			fprintf(stderr , " Usage : %s < File To Be Execute \n" , argv[0] );
+		}
 
+		execve(argv[1] , newargv , NULL);
 }
