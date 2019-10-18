908a899fe22203419e4b3b42e221d2785b2eb325
Prathamesh J. Chatorikar <51926984+Pratham19Feb@users.noreply.github.com>
2019-10-18 21:28:15 +0530

    Update DP.c

--- Assignment 6/DP.c
+++ Assignment 6/DP.c
@@ -1,100 +1,124 @@
-#include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
 #include <unistd.h>
-#define THINKING 0
-#define EATING 1
-#define HUNGRY 2
-#define Total_Philospher 5
-#define left (Philosopher_Num + 4) % Total_Philospher
-#define right (Philosopher_Num + 1) % Total_Philospher
+#include <semaphore.h>
+
+// Philosopher Number
+#define N 5
 
 
-sem_t mutex;
-sem_t S[Total_Philospher];
-
-
-int Philosopher_state[Total_Philospher];
-int phil[Total_Philospher] = { 0, 1, 2, 3, 4 };
-
-void TEST(int Philosopher_Num)
+enum
 {
-  if( Philosopher_state[Philosopher_Num] == HUNGRY && Philosopher_state[left] != EATING && Philosopher_state[right] != EATING )
-  {
-    sleep(2);
-    printf("Philosopher %d takes fork %d and %d\n", Philosopher_Num , left + 1, right + 1);
+    THINKING ,
+    EATING ,
+    HUNGRY
+}state[N];
 
-    printf("Philosopher %d is Eating\n", Philosopher_Num + 1);
-    sem_post(&S[Philosopher_Num]);
-  }
-}
+int eatcount[N] ={0};
+pthread_mutex_t lock;
+sem_t chopstick[N];
 
-
-void Take_ChopStick(int Philosopher_Num)
+void think(int i)
 {
-    sem_wait(&mutex);
-
-    printf("Philosopher %d is Hungry\n", Philosopher_Num + 1);
-
-    sem_post(&mutex);
-
-    sem_wait(&S[Philosopher_Num]);
-
-    sleep(1);
-}
-
-
-void Put_Fork(int Philosopher_Num)
-{
-  sem_wait(&mutex);
-
-   Philosopher_state[Philosopher_Num] = THINKING;
-
-   printf("Philosopher %d putting fork %d and %d down\n",  Philosopher_Num + 1, left + 1, Philosopher_Num + 1);
-   printf("Philosopher %d is thinking\n", Philosopher_Num + 1);
-
-   TEST(left);
-   TEST(right);
-
-   sem_post(&mutex);
-}
-
-void* philospher(void* num)
-{
-
-    while (1) {
-
-        int* i = num;
-
-        sleep(1);
-        printf("Eating %d" , num);
-        Take_ChopStick(*i);
-
-        // printf("Eating %" , num);
-        sleep(0);
-
-        Put_Fork(*i);
+    if(state[i] == THINKING)
+    {
+        int temp = rand()%3;
+        if(temp == 0)
+            temp = 2;
+        printf("\n Philospher %d : Thinking \n" , i+1);
+        sleep(temp);
+        state[i] = HUNGRY;
     }
 }
 
+void takechopstic(int i)
+{
+    sem_wait(&chopstick[i]);
+    sem_wait(&chopstick[(i+1)%N]);
+}
+
+void putchopstick(int i)
+{
+    sem_post(&chopstick[i]);
+    sem_post(&chopstick[(i+1)%N]);
+
+}
+
+void eating(int i)
+{
+    int temp = rand()% 3;
+    if(temp == 0)
+        temp = 2;
+    
+    if( state[i] == HUNGRY && state[(i+1)%N] != EATING && state[(i + N-1)%N] != EATING)
+    {
+        state[i] = EATING;
+        printf("\n\n Philopher %d Eating ",i+1);
+        takechopstic(i);
+
+        sleep(temp);
+        
+        eatcount[i]++;
+        printf("\n\n %d Done with Eating " ,i+1);
+
+        putchopstick(i);
+        state[i] = THINKING;
+
+        pthread_mutex_lock(&lock);
+        printf("\n\n\n ==========================");
+        printf("\n\n EatCount is  [  ");
+
+        for(int j = 0 ; j < N ; j++)
+        {
+            printf(" %d" , eatcount[j]);
+        }
+        printf("  ] ");
+        printf("\n\n\n ==========================");
+        pthread_mutex_unlock(&lock);
+    }
+}
+
+void * philospher(void * num)
+{
+     int i = *(int*)num;
+     
+    while(1)
+    {
+        think(i);
+        eating(i);
+    }
+}
+
+
 int main()
 {
-  int i;
-  pthread_t thread_id[Total_Philospher];
+    int *arg  , result , i;
+    pthread_mutex_init(&lock,NULL);
+    pthread_t philosphertherad;
 
+    for(int i = 0 ; i < N ; i++ )
+    {
+        sem_init(&chopstick[i] , 0 , 1);
+    }
 
-  sem_init(&mutex, 0, 1);
+    for( i =  0 ; i < N ;i++)
+    {
+        arg = (int *)malloc(sizeof(int));
+        *arg = i;
+        pthread_create(&philosphertherad , NULL , philospher , (void *)arg);
+    }
 
-  for (i = 0; i < Total_Philospher; i++)
+    for(i = 0 ; i < N ; i++)
+    {
+        if( pthread_join(philosphertherad,NULL) != 0)
+        {
+            printf("\n\n Error in Thhread joining");
+        }
+        
 
-      sem_init(&S[i], 0, 0);
+    }
 
-  for (i = 0; i < Total_Philospher; i++) {
-      pthread_create(&thread_id[i], NULL,   philospher, &phil[i]);
-
-      printf("Philosopher %d is thinking\n", i + 1);
-  }
-
-  for (i = 0; i < Total_Philospher; i++)
-      pthread_join(thread_id[i], NULL);
+    
 }
