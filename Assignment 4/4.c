8e3f3e3000d9465c478dfe6c2f5d971294f24410
Prathamesh J. Chatorikar <51926984+Pratham19Feb@users.noreply.github.com>
2019-10-19 11:14:03 +0530

    Reader Writer

--- Assignment 4/4.c
+++ Assignment 4/4.c
@@ -1,131 +1,114 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+#include<semaphore.h>
+#include<pthread.h>
 
-/*
-use the pthread flag with gcc to compile this code
-~$ gcc -pthread producer_consumer.c -o producer_consumer
-*/
+int readcount = 0;
+sem_t mutex  , rwlock;
+int data=0;
+void *reader(void *arg);
+void *writer(void *arg);
+int c = 0;
+int main()
+{
+    int r =  5 , w = 5 , i  , j , *arg;
+    
+    pthread_t readerT , writerT;
+    sem_init(&mutex , 0 , r);
+    sem_init(&rwlock,0,1);
+
+    for(i = 0 ; i < r ; i++)
+    {
+        arg = (int *)malloc(sizeof(int));
+        *arg = i;
+        pthread_create(&readerT , NULL , reader , arg);
+    }
+
+    for(i = 0 ; i < w ; i++)
+    {
+        arg = (int *)malloc(sizeof(int));
+        *arg = i;
+        pthread_create( &writerT , NULL , writer , arg);
+    }
+
+    pthread_join(readerT,NULL);
+    pthread_join(writerT,NULL);
+    
+}
+void *reader(void *arg)
+{
+    int *num = (int *)arg;
+    int temp;
+    temp = rand()%3;
+        if(temp == 0)
+            temp = 2;
+    while(1)
+    {
+        
+        sleep(temp);
+       
+        sem_wait(&mutex);
+
+        printf("\n\n {  %d  }   Reader Before The Crtical Section",*num);
+
+        readcount++;
+        if(readcount == 1)
+            sem_wait(&rwlock);
+        
+        
+
+        sem_post(&mutex);
+        c++;
+        printf("\n\nNo. of People  in CS :  %d   :  [   %d   ] In critical Section Reader reads data  ---> ((  %d  ))" ,c , *num ,data );
+        
+        sem_wait(&mutex);
+
+        sleep(1);
+         c--;
+        printf("\n\n %d (  %d  ) Leaves Crtical Section " ,c , *num);
+        
+        readcount--;
+       
+        if(readcount == 0)
+            sem_post(&rwlock);
+        sem_post(&mutex);
+    }
 
 
-pthread_t *producers;
-pthread_t *consumers;
-
-sem_t buf_mutex,empty_count,fill_count;
-
-int *buf,buf_pos=-1,prod_count,con_count,buf_len;
-
-
-int produce(pthread_t self){
-	int i = 0;
-	int p = 1 + rand()%40;
-	while(!pthread_equal(*(producers+i),self) && i < prod_count){
-		i++;
-	}
-	printf("Producer %d produced %d \n",i+1,p);
-	return p;
 }
 
+void *writer( void *arg)
+{
+    int *num = (int *)arg;
+    int temp;
 
-void consume(int p,pthread_t self){
-	int i = 0;
-	while(!pthread_equal(*(consumers+i),self) && i < con_count){
-		i++;
-	}
+    while(1)
+    {
+        temp = rand()%3;
+        if(temp == 0)
+            temp = 2;
+        sleep(temp);
 
-	printf("Buffer:");
-	for(i=0;i<=buf_pos;++i)
-		printf("%d ",*(buf+i));
-	printf("\nConsumer %d consumed %d \nCurrent buffer len: %d\n",i+1,p,buf_pos);
+        sem_wait(&rwlock);
 
+        printf("\n\n\t\t\t\t\t\t\t Writer Enter into Critical Section %d", *num);
+        //sleep(temp);
+        data++;
+        
+        c++;
+        
+        printf("\n\n\t\t\t\t\t\t\t No. of People  in CS :  %d   :  Writer {   %d  }  writes data  ====>   [   %d   ] " ,c ,*num, data);
+        
+        c--;
+        
+        printf("\n\n\t\t\t\t\t\t\tNo. of People  in CS :  %d   :   Writer [  %d ] leaves crtical Section   " ,c, *num);
+
+        
+        sem_post(&rwlock);
+    }
 }
 
-
-void* producer(void *args){
-
-	while(1){
-		int p = produce(pthread_self());
-		sem_wait(&empty_count);
-		sem_wait(&buf_mutex);
-		++buf_pos;			// critical section
-		*(buf + buf_pos) = p;
-		sem_post(&buf_mutex);
-		sem_post(&fill_count);
-		sleep(1 + rand()%3);
-	}
-
-	return NULL;
-}
-
-
-void* consumer(void *args){
-	int c;
-	while(1){
-		sem_wait(&fill_count);
-		sem_wait(&buf_mutex);
-		c = *(buf+buf_pos);
-		consume(c,pthread_self());
-		--buf_pos;
-		sem_post(&buf_mutex);
-		sem_post(&empty_count);
-		sleep(1+rand()%5);
-	}
-
-	return NULL;
-}
-
-int main(void){
-
-	int i,err;
-
-	srand(time(NULL));
-
-	sem_init(&buf_mutex,0,1);
-	sem_init(&fill_count,0,0);
-
-	printf("Enter the number of Producers:");
-	scanf("%d",&prod_count);
-	producers = (pthread_t*) malloc(prod_count*sizeof(pthread_t));
-
-	printf("Enter the number of Consumers:");
-	scanf("%d",&con_count);
-	consumers = (pthread_t*) malloc(con_count*sizeof(pthread_t));
-
-	printf("Enter buffer capacity:");
-	scanf("%d",&buf_len);
-	buf = (int*) malloc(buf_len*sizeof(int));
-
-	sem_init(&empty_count,0,buf_len);
-
-	for(i=0;i<prod_count;i++){
-		err = pthread_create(producers+i,NULL,&producer,NULL);
-		if(err != 0){
-			printf("Error creating producer %d: %s\n",i+1,strerror(err));
-		}else{
-			printf("Successfully created producer %d\n",i+1);
-		}
-	}
-
-	for(i=0;i<con_count;i++){
-		err = pthread_create(consumers+i,NULL,&consumer,NULL);
-		if(err != 0){
-			printf("Error creating consumer %d: %s\n",i+1,strerror(err));
-		}else{
-			printf("Successfully created consumer %d\n",i+1);
-		}
-	}
-
-	for(i=0;i<prod_count;i++){
-		pthread_join(*(producers+i),NULL);
-	}
-	for(i=0;i<con_count;i++){
-		pthread_join(*(consumers+i),NULL);
-	}
-
-
-	return 0;
-}
